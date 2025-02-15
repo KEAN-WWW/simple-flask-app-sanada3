81c18350c8fdbc41a09138ee43ce35e4ad189126
sanada3 <charlyverno@gmail.com>
2025-02-15 16:27:50 -0500

    complete flask assignment first push

--- tests/test_valid_user.py
+++ tests/test_valid_user.py
@@ -1,15 +1,20 @@
-"""This is a test script to test flask application"""
+"""This module contains test cases for the valid user route."""
+# Third party imports
 import pytest
+
+# Local application imports
 from app.app import app
+
 
 @pytest.fixture(name="client")
 def create_client():
-    """initialize a fixture test client for flask unit testing"""
+    """Initialize a fixture test client for flask unit testing."""
     with app.test_client() as app_client:
         yield app_client
 
+
 def test_valid_user_content(client):
-    """flask unit testing for content in valid parameter page"""
+    """Flask unit testing for content in valid parameter page."""
     response = client.get("/user/jack")
     assert response.status_code == 200
-    assert b"jack".upper() in response.data
+    assert b"JACK" in response.data
